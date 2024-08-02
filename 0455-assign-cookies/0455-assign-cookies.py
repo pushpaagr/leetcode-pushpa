1b4cc6d1249a4da005f3a57a71b09ca09a7bf601
Pushpa <pushpaagr108@gmail.com>
2024-08-01 17:03:12 -0700

    Time: 134 ms (49.07%), Space: 18.4 MB (75.87%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        # g = array of child greeds
+        # s = array of cookie sizes
+        g.sort()
+        s.sort() 
+        
+        
+        child = 0
+        cookie = 0
+        
+        while cookie < len(s) and child < len(g):
+            if s[cookie] >= g[child]:
+                child += 1
+            cookie += 1
+        
+        return child
+        
+    # Time: O(NlogN + MlogM)
+    # Space: O(N+M) (because sorting in python takes O(N) space)
+
+        
