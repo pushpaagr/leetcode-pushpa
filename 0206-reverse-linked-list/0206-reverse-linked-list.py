7fb6067dfdcb97cd98a087b92ed8cc3675e6e3ec
Pushpa <pushpaagr108@gmail.com>
2025-01-27 10:56:48 -0800

    Time: 0 ms (100%), Space: 18.7 MB (38.75%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
@@ -12,6 +11,6 @@ class Solution:
         while curr:
             nxt = curr.next
             curr.next = prev
-            prev = curr
+            prev = curr 
             curr = nxt
         return prev
